01afa7999f916ba643bbc278cab47939ca812b20
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-10-13 10:15:31 +0200

    added comment for Silas' path

--- testFile.py
+++ testFile.py
@@ -4,6 +4,9 @@ import numpy as np
 #Hans path (comment out if not Hans)
 #path = r"C:\Users\hansh\OneDrive - Aalborg Universitet\Programmer\3. semester\RSP\Miniproject\Training set\1.jpg"
 
+#Hans path (comment out if not Hans)
+#path = ---- ADD PATH HERE ----
+
 testImg = cv.imread(path, 1)
 cv.imshow("picture",testImg)
 cv.waitKey(0)
