61f99798843cc4d93f336043616c72d47a3bd852
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-10-13 10:59:10 +0200

    add comments to segment function

--- croppedImgPointCalculator.py
+++ croppedImgPointCalculator.py
@@ -8,13 +8,23 @@ path = r"C:\Users\hansh\OneDrive - Aalborg Universitet\Programmer\3. semester\RS
 #path = ---- ADD PATH HERE ----
 
 def segment(img):
+    """segments image into 5x5 tiles based on height and with
+
+    Args:
+        img (mat): image (either 3D matrix or 2D matrix)
+
+    Returns:
+        array: a 5x5 array width an image in each index
+    """
+    #calculates the height and width of each segment when images is split into 25 equal areas (5x5)
     segmentWidth = int(img.shape[1]/5)
     segmentHeight= int(img.shape[0]/5)
+    #initializes output array 
     output = []
     for segmentY in range(5):
-        output.append([])
+        output.append([]) #added line of images
         for segmentX in range(5):
-            output[segmentY].append( img[segmentY*segmentHeight:(segmentY+1)*segmentHeight,segmentX*segmentWidth:(segmentX+1)*segmentWidth])
+            output[segmentY].append( img[segmentY*segmentHeight:(segmentY+1)*segmentHeight,segmentX*segmentWidth:(segmentX+1)*segmentWidth]) # inputs image into line
     return output 
             
 testImg = cv.imread(path, 1)
