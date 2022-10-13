3783fe8875170533d767516b98e8f101718eeb52
SilasStensgaard <56107703+SilasStensgaard@users.noreply.github.com>
2022-10-13 13:26:53 +0200

    Creating loop to perform closings of the image

--- colorDetection.py
+++ colorDetection.py
@@ -30,10 +30,30 @@ def lowUpBound(hueLow,hueUp,satLow, satUp, valLow, valUp):
 
 #I have found values for the grasslands and have created lower and upper bounds 
 grassland = lowUpBound(40,100,50,100,50,100)
+#The mask uses the lower and upper bounds to tell wether to save the pixel or not
 mask = cv2.inRange(hsv,grassland[0], grassland[1])
-print(mask)
+#This uses the mask to check for the colors then only shows the colors that fit within the threshold
 result = cv2.bitwise_and(img, img, mask=mask)
+
+#Turning the image binary so that "closing" can be performed
+result_gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+(thresh, blackAndWhiteImage) = cv2.threshold(result_gray, 10, 255, cv2.THRESH_BINARY)
+
+#You determine the maximum size of the kernel you want to use
+kernelSize = 42
+#The loop goes through many closings of increasing sizes
+#This should remove the small mess in the begining while closing the big holes in the end
+for x in range(kernelSize):
+    global closing
+    #The size of the kernel changes along with how far the loop is
+    kernel = np.ones([x+1,x+1],np.uint8)
+    #The closing is then performed
+    closing = cv2.morphologyEx(blackAndWhiteImage, cv2.MORPH_CLOSE, kernel)
+
+#Showing all the images
+cv2.imshow('binary',blackAndWhiteImage)
 cv2.imshow('result',result)
+cv2.imshow('closing', closing)
 cv2.waitKey()
 
 
