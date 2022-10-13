e5d6cf269284796d0be147bf62f1fd9d2a6815a6
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-10-13 10:47:09 +0200

    added segment function

--- croppedImgPointCalculator.py
+++ croppedImgPointCalculator.py
@@ -7,6 +7,16 @@ path = r"C:\Users\hansh\OneDrive - Aalborg Universitet\Programmer\3. semester\RS
 #Hans path (comment out if not Hans)
 #path = ---- ADD PATH HERE ----
 
+def segment(img):
+    segmentWidth = int(img.shape[1]/5)
+    segmentHeight= int(img.shape[0]/5)
+    output = []
+    for segmentY in range(5):
+        output.append([])
+        for segmentX in range(5):
+            output[segmentY].append( img[segmentY*segmentHeight:(segmentY+1)*segmentHeight,segmentX*segmentWidth:(segmentX+1)*segmentWidth])
+    return output 
+            
 testImg = cv.imread(path, 1)
 
 cv.imshow("picture",testImg)
