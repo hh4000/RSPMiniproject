3f2c293901ab38a9a2cfe65bfb48fae0cdfbb932
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-10-13 10:14:34 +0200

    pathing example added

---
+++ testFile.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+
+#Hans path (comment out if not Hans)
+#path = r"C:\Users\hansh\OneDrive - Aalborg Universitet\Programmer\3. semester\RSP\Miniproject\Training set\1.jpg"
+
+testImg = cv.imread(path, 1)
+cv.imshow("picture",testImg)
+cv.waitKey(0)
