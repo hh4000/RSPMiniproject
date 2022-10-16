aec771018072d4e01634b99dca3b12b345828199
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-10-16 11:08:15 +0200

    added imports

---
+++ tileDetection.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import numpy as np
+
+#import commands and variables from colorDetection
+from colorDetection import closedGrassland
+from colorDetection import closedOcean
+from colorDetection import closedForest
+from colorDetection import closedDesert
+from colorDetection import closedSwamp
+from colorDetection import closedMountain
+
