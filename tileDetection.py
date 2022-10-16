e712cdc334506186726e506b0e34b9da3082c384
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-10-16 11:44:31 +0200

    added comments

--- tileDetection.py
+++ tileDetection.py
@@ -11,24 +11,39 @@ from colorDetection import closedSwamp
 from colorDetection import closedMountain
 
 def identifyTiles():#must be changed to take argument (for varying input image)
+    """From a cropped, perspective corrected image of a board, identifies the types of tiles present on the board.
+    
+
+    Returns:
+        numpy.chararray: char array with identified tile types. Grasslands = 'g', Ocean = 'o', Forest = 'f', Desert = 'd', Swamp = 's', Mountain = 'm', Error = 'E', Unidentified = 'Ø'
+    """
+    #array of closed colordetected image
     tileTypes = [closedGrassland,closedOcean,closedForest,closedDesert,closedSwamp,closedMountain]
+    #array of characters used to identify the tile types - grasslands are 'g', oceans are 'o', etc.
     tileChar = ['g','o','f','d','s','m']
+    
+    #initializes output array with all inputs as ""
     tiles = np.chararray((5,5))
     tiles[:] = ""
     
-    for n, tileType in enumerate(tileTypes):
-        segmentedBoard = segment(tileType)
-        for y, imgRow in enumerate(segmentedBoard):
-            for x, img in enumerate(imgRow):
-                if np.median(img) == 255:
+    for n, tileType in enumerate(tileTypes):#checks for each tile type
+        segmentedBoard = segment(tileType)#segments into 25 (5x5) pieces
+        for y, imgRow in enumerate(segmentedBoard): #checks each row of board
+            for x, img in enumerate(imgRow):#checks each tile of row
+                
+                if np.median(img) == 255: #checks if tyle is of current tile Type - pressumed if >50% of image is white that it is of current type
+                    #checks if current tile has already has a identified tile type. if it has, type is changed to 'E' (error)
                     if tiles[y,x] != "":
                         tiles[y,x] = "E"
                     else:
+                    #if no error type is set accordingly
                         tiles[y,x] = tileChar[n]
-    for tileRow in tiles:
-        for tile in tileRow:
+                        
+    # checks if any tiles are unidentfied. In that case, type is set as 'Ø'
+    for y, tileRow in enumerate(tiles):
+        for x, tile in enumerate(tileRow):
             if tile == "":
-                tile = "Ø"
+                tiles[y,x] = "Ø"
     return tiles
 
 print(identifyTiles())
