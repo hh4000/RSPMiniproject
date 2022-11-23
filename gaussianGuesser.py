aca5017c5ad259ab085a2053d9461321eac9e557
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-11-23 13:29:14 +0100

    Added logic statement so only one tile is chosen as home tile (highest percieved likelihood)

--- gaussianGuesser.py
+++ gaussianGuesser.py
@@ -3,18 +3,33 @@ import numpy as np
 import cv2 as cv
 import dataComparison as dc
 
-path = r"C:\Users\hansh\OneDrive - Aalborg Universitet\Programmer\3. semester\RSP\Miniproject\Training set\17.jpg"
+path = r"C:\Users\hansh\OneDrive - Aalborg Universitet\Programmer\3. semester\RSP\Miniproject\TrainingCropped\66.jpg"
 
 inputImg = cv.imread(path,1)
-tileArr = [dc.forest,dc.ocean,dc.grass,dc.swamp,dc.mountain,dc.wheat,dc.home,dc.forestC,dc.oceanC,dc.grassC,dc.swampC,dc.mountainC,dc.wheatC]
+tileArr = [dc.forest,dc.ocean,dc.grass,dc.swamp,dc.mountain,dc.wheat,dc.null]
 def guessTiles(image):
     segmentedImage = segment(image)
     result = np.chararray((5,5))
+    
+    #This part no worke
+    HPose = []
+    highestlikelihood = -np.inf
     for y,imgrow in enumerate(segmentedImage):
         for x,img in enumerate(imgrow):
-            likelihoodArr = [tile.calcLogLikelihood(img) for tile in tileArr ]
-            maxIndex = likelihoodArr.index(max(likelihoodArr))
-            result[y,x] = tileArr[maxIndex].name
+            likelihood = dc.home.calcLogLikelihood(img)
+            if likelihood > highestlikelihood:
+                highestlikelihood = likelihood
+                HPose = [y,x]
+    y,x=HPose
+    result[y,x] = dc.home.name
+    print((np.max(highestlikelihood)))
+    for y,imgrow in enumerate(segmentedImage):
+        for x,img in enumerate(imgrow):
+            if result[y,x] != b"H":
+                likelihoodArr = [tile.calcLogLikelihood(img) for tile in tileArr ]
+                likelihoodArr[6] = likelihoodArr[6]-np.log(5)
+                maxIndex = likelihoodArr.index(max(likelihoodArr))
+                result[y,x] = tileArr[maxIndex].name
     return result
 
 print(guessTiles(inputImg))
